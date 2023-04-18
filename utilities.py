af602bc1b066845ac0a3838dcde285e2ddd78eec
Jan Misiurek <jan.misiurek@gmail.com>
2023-04-18 22:00:14 +0200

    add chromedriver_autoinstaller

--- utilities.py
+++ utilities.py
@@ -3,29 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+import chromedriver_autoinstaller
 import time
 
 def login_to_twitter():
+    chromedriver_autoinstaller.install()
+
     email = os.environ['EMAIL']
     username = os.environ['TWITTER_USERNAME']
     password = os.environ['TWITTER_PASSWORD']
 
-    #checking env vakues
-    print(f"Chrome binary: {chrome_bin}")
-    print(f"Chrome driver: {chrome_driver_path}")
-
-    chrome_driver_path = os.environ.get("GOOGLE_CHROME_SHIM", None)
-    chrome_bin = os.environ.get("CHROMEDRIVER_PATH", None)
-    service = Service(executable_path=chrome_driver_path)
-
+    #chrome_driver_path = os.environ.get("CHROMEDRIVER_PATH", None)
+    #chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+    
     chrome_options = Options()
-    chrome_options.binary_location = chrome_bin
+    #chrome_options.binary_location = chrome_bin
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+   #driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
+    # ...
     driver.get('https://twitter.com/i/flow/login')
     driver.implicitly_wait(10)
     time.sleep(3)
