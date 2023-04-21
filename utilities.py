1b57fa7ada430f910dc359a98eb8db8bc8ac64ab
Jan Misiurek <jan.misiurek@gmail.com>
2023-04-21 08:30:59 +0200

    chrome options fix

--- utilities.py
+++ utilities.py
@@ -20,11 +20,11 @@ def login_to_twitter():
     #chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
     
     chrome_options = Options()
-    #chrome_options.binary_location = chrome_bin
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-gpu")
-    #chrome_options.add_argument("--no-sandbox")
-    #chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = chrome_bin
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     
    #driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
     driver = webdriver.Chrome(options=chrome_options)
