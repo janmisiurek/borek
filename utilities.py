8b6b86e5e663b09ab7380c56b7fa8bce3d1c0c45
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-05 14:52:46 +0200

    remove starting driver from login

--- utilities.py
+++ utilities.py
@@ -9,29 +9,24 @@ import pandas as pd
 import time
 import openai
 
+chromedriver_autoinstaller.install() 
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
+
 def login_to_twitter():
-    chromedriver_autoinstaller.install()
+    
 
     email = os.environ['EMAIL']
     username = os.environ['TWITTER_USERNAME']
     password = os.environ['TWITTER_PASSWORD']
     number = os.environ['NUMBER']
-    
 
-    #chrome_driver_path = os.environ.get("CHROMEDRIVER_PATH", None)
-    #chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-    
-    chrome_options = Options()
-    #chrome_options.binary_location = chrome_bin
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    
-   #driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
-    print("Before initializing driver")
-
-    driver = webdriver.Chrome(options=chrome_options)
     # ...
     print("After initializing driver")
     driver.get('https://twitter.com/i/flow/login')
