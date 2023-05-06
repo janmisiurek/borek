c9a1242649be3d389648f8cbfcc1ed9f83eaef05
Jan Misiurek <jan.misiurek@gmail.com>
2023-05-06 20:07:05 +0200

    chrome oprions fix

--- utilities.py
+++ utilities.py
@@ -12,10 +12,10 @@ import openai
 chromedriver_autoinstaller.install() 
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
 
